ed9fabaf6b4b45f50db35369392ce45e1e3ff7cf
Toby Maddix <tobymaddix@gmail.com>
2022-03-12 14:29:45 +0000

    +Display errors with postcode

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     function getStats(statsData) {
-        $.getJSON(statsData, function(data){
+        $.get(statsData, function(data){
             $("#statsListed").text("Listed Spaces - " + data.dev_stats.spaces_listed);
             $("#statsComplete").text("Complete - " + data.dev_stats.spaces_complete);
             $("#statsDetailed").text("Detailed - " + data.dev_stats.spaces_detailed);
@@ -15,18 +15,15 @@ $(document).ready(function(){
     function getPostcode(position) {
         const latlongUrl = "https://api.postcodes.io/postcodes?lon=" + position.coords.longitude + "&lat=" + position.coords.latitude;
 
-        $.getJSON(latlongUrl, function(data) {
+        $.get(latlongUrl, function(data) {
             console.log(data);
+            console.log("SUCCESS! " + data.status);
+            console.log(data.result[1].postcode);
 
-            if(data.responce = 200) {
-                console.log("SUCCESS! " + data.status);
-                console.log(data.result[1].postcode);
-
-                $("#searchField").val(data.result[1].postcode);
-            } else {
-                console.log("ERROR! " + data.status);
-            }
-
+            $("#searchField").val(data.result[1].postcode);
+        }).fail(function(data){
+            console.log(data);
+            console.log("ERROR! " + data.status);
         });
     }
 
@@ -36,54 +33,63 @@ $(document).ready(function(){
         if(navigator.geolocation) {navigator.geolocation.getCurrentPosition(getPostcode);}
     });
 
+    
+    //remove any content from main
+    function clearMain() {
+        for(var i = 1; $("#main").length+1 >= i; i++){
+            $("#mainRow" + i).remove();
+        }
+    }
 
-    function find() {
+    function getSpaces() {
         var postcode = $("#searchField").val();
         var location;
     
         const postcodeUrl = "https://api.postcodes.io/postcodes/" + postcode;
     
-        $.getJSON(postcodeUrl, function(data) {
-            location = data;
-            console.log(location);
-    
-            if(location.responce = "200") {
-                console.log("SUCCESS! " + location.status);
-                console.log(location.result.longitude);
-                console.log(location.result.latitude);
+        $.get(postcodeUrl, function(data) {
+            console.log(data);
+            console.log("SUCCESS! " + data.status);
+            console.log(data.result.longitude);
+            console.log(data.result.latitude);
 
-                function displayResult(name, latitude, longitude, row, col) {
-                    var eRow = document.getElementById("mainRow" + row);
-                    if (eRow == null) {$("#main").append("<div class='mainRow' id='mainRow" + row + "'></div>");}
-                    var eCol = document.getElementById("mainRow" + row + "Col" + col);
-                    if (eCol == null) {$("#mainRow" + row).append("<div class='mainColLowlight' id='mainRow" + row + "Col" + col + "'></div>");}
-                    $("#mainRow" + row + "Col" + col).append("<p>" + name + "</p>");
-                    $("#mainRow" + row + "Col" + col).append("<p>" + latitude + "</p>");
-                    $("#mainRow" + row + "Col" + col).append("<p>" + longitude + "</p>");
-                }
-                
-                for(var i = 1; $("#main").length+1 >= i; i++){
-                    $("#mainRow" + i).remove();
-                }
-                displayResult(location.result.postcode, location.result.longitude, location.result.latitude, 1, 1);
-                displayResult(location.result.postcode, location.result.longitude, location.result.latitude, 1, 2);
-                displayResult(location.result.postcode, location.result.longitude, location.result.latitude, 2, 1);
-                displayResult(location.result.postcode, location.result.longitude, location.result.latitude, 2, 2);
-                displayResult(location.result.postcode, location.result.longitude, location.result.latitude, 2, 3);
-            } else {
-                console.log("ERROR! " + location.status);
+            //animate to default colors if success
+            $("#searchField").css({"background-color":"rgba(250,250,250,1)", "color":"rgba(80, 80, 80, 1)"});
+
+            function displayResult(name, latitude, longitude, row, col) {
+                var eRow = document.getElementById("mainRow" + row);
+                if (eRow == null) {$("#main").append("<div class='mainRow' id='mainRow" + row + "'></div>");}
+                var eCol = document.getElementById("mainRow" + row + "Col" + col);
+                if (eCol == null) {$("#mainRow" + row).append("<div class='mainColLowlight' id='mainRow" + row + "Col" + col + "'></div>");}
+                $("#mainRow" + row + "Col" + col).append("<p>" + name + "</p>");
+                $("#mainRow" + row + "Col" + col).append("<p>" + latitude + "</p>");
+                $("#mainRow" + row + "Col" + col).append("<p>" + longitude + "</p>");
             }
+            
+            clearMain();
+            displayResult(data.result.postcode, data.result.longitude, data.result.latitude, 1, 1);
+            displayResult(data.result.postcode, data.result.longitude, data.result.latitude, 1, 2);
+            displayResult(data.result.postcode, data.result.longitude, data.result.latitude, 2, 1);
+            displayResult(data.result.postcode, data.result.longitude, data.result.latitude, 2, 2);
+            displayResult(data.result.postcode, data.result.longitude, data.result.latitude, 2, 3);
+        }).fail(function(data){ 
+            console.log(data);
+            console.log("ERROR! " + data.status);
+
+            //animate color change to show fail
+            $("#searchField").css({"background-color":"rgba(200,40,20,1)", "color":"white"});
+            clearMain();
         });
     }
 
     $("#searchButton").click(function(){
-        find();
+        getSpaces();
     });
 
     $('#searchField').keypress(function(event){
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if(keycode == '13'){
-            find();
+            getSpaces();
         }
       });
 });
